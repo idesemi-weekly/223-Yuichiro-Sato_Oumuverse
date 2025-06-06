8eb49e21f8ac7a7738cad95e6aec840dd03f5eb1
sato <22311092ys@tama.ac.jp>
2025-06-06 13:56:51 +0900

    add: 書くHTMLファイルの追加とルーティングの追加

--- src/app.py
+++ src/app.py
@@ -6,3 +6,23 @@ app = Flask(__name__)
 @app.route("/toppage")
 def toppage():
     return render_template("toppage.html")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
+@app.route("/everyonepost")
+def everyonepost():
+    return render_template("everyonepost.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/mypost")
+def mypost():
+    return render_template("mypost.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
