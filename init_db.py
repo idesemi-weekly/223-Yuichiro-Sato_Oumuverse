d735b6b307a688dba64c39add2124fcd5bf11cc9
sato <y177649@os3-382-24272.vs.sakura.ne.jp>
2025-06-12 20:35:01 +0900

    Fix: DBが正常に生成できない問題と、gunicornがDBを参照できない問題に対応

---
+++ init_db.py
@@ -0,0 +1,10 @@
+from src.app import app, db
+import sys
+
+try:
+    with app.app_context():
+        db.create_all()
+        print("データベースの初期化に成功しました")
+except Exception as e:
+    print(f"データベースの初期化に失敗しました: {e}", file=sys.stderr)
+    sys.exit(1)
